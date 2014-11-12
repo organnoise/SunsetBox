52d7e1506c0b155fc7d794b05cf4a3d123dc7334
organnoise <dm09ke@student.ocadu.ca>
2014-11-12 16:04:18 -0500

    intial commit

---
+++ ArduinoRGB_control/RGB_control.ino
@@ -0,0 +1,33 @@
+#define LEDR     9
+#define LEDG     6
+#define LEDB     5
+
+int r, g, b;
+
+void setup()
+{
+  bitSet(TCCR1B, WGM12);
+  pinMode(LEDR, OUTPUT);
+  pinMode(LEDG, OUTPUT);
+  pinMode(LEDB, OUTPUT);
+  r = g = b = 0;
+  
+  Serial.begin(9600);
+  }
+
+void loop()
+{
+   
+  
+  if(Serial.available() >= 3)
+  {
+    r = Serial.read();
+    g = Serial.read();
+    b = Serial.read();
+    Serial.write('1'); // Sync
+  }
+ 
+  analogWrite(LEDR, r);
+  analogWrite(LEDG, g);
+  analogWrite(LEDB, b);
+}
